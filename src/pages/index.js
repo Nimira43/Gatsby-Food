6587231dda2f13560b3c8fcd00d8ee5c5fb0f88b
Nimira43 <n.rabone72@aol.com>
2025-04-23 21:59:09 +0100

    Creating pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
-import * as React from "react"
+import * as React from 'react'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>Gatsby Food</h1>
+  </div>
 }
