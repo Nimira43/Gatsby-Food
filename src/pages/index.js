fac7059943c1a0be1f42ebbdd78e7651dba5282b
Nimira43 <n.rabone72@aol.com>
2025-04-24 05:15:51 +0100

    Created index.css. Did CSS resets, imported and set up fonts.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import Layout from '../components/Layout'
+import '../index.css'
 
 export default function Home() {
   return (
     <Layout>
       <div>
-        <h1>Gatsby Food Company</h1>
+        <h1 className='logo'>Gatsby Food</h1>
         <img src="/favicon.ico" alt="logo" width={100} height={80} />
       </div>
     </Layout>
