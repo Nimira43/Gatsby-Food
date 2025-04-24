06ff7804cb52ca719243d1dc646e1044633c2348
Nimira43 <n.rabone72@aol.com>
2025-04-24 04:23:45 +0100

    Added navbar and logo image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,16 @@ import { Link } from 'gatsby'
 export default function Home() {
   return (
     <div>
-      <h1>Gatsby Food Company</h1>
-      <hr />
-      <Link to='/projects'>Projects</Link>
-      <Link to='/about'>About</Link>
-      <Link to='/contact'>Contact</Link>
-      <Link to='/resources'>Resources</Link>
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='/projects'>Projects</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/contact'>Contact</Link>
+      </nav>
+      <div>
+        <h1>Gatsby Food Company</h1>
+        <img src="/favicon.ico" alt="logo" width={100} height={80} />
+      </div>
     </div>
   )
 }
