45711c34b173f511f623d73377f9535628e1a3bc
Nimira43 <n.rabone72@aol.com>
2025-04-24 03:41:13 +0100

    Nested routes

---
+++ src/pages/projects/project1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const project1 = () => {
+  return (
+    <div>
+      Project 1
+    </div>
+  )
+}
+
+export default project1
---
+++ src/pages/projects/project2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const project2 = () => {
+  return (
+    <div>
+      Project 2
+    </div>
+  )
+}
+
+export default project2
