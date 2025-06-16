afe46445c767b13228b4c0f261d997fb72c91001
Nimira43 <n.rabone72@aol.com>
2025-06-16 07:37:52 +0100

    Setting up bg colours for home and about pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,9 +4,11 @@ import Layout from '../components/Layout'
 const about = () => {
   return (
     <Layout>
-      <h1>About</h1>
-      <hr />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium in libero assumenda, eum facere aliquid natus placeat ipsa numquam! Repellat?</p>
+      <div className='about-bg'>
+        <h1>About</h1>
+        <hr />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium in libero assumenda, eum facere aliquid natus placeat ipsa numquam! Repellat?</p>  
+      </div>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout>
-      <div>
+      <div className='home-bg'>
         <h1 className='logo'>Gatsby Food</h1>
         <img src="/favicon.ico" alt="logo" width={100} height={80} />
       </div>
