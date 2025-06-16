a891b14ad6c41e6345402dea829a9ffe5a7a4062
Nimira43 <n.rabone72@aol.com>
2025-06-16 08:01:03 +0100

    Added same classes to individual projects.

--- src/pages/projects/project2.js
+++ src/pages/projects/project2.js
@@ -4,9 +4,10 @@ import Layout from '../../components/Layout'
 const project2 = () => {
   return (
     <Layout>
-      <h1>Project 2</h1>
-      <hr />
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium in libero assumenda, eum facere aliquid natus placeat ipsa numquam! Repellat?</p>
+      <div className="content projects-bg">
+        <h1>Project 2</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium in libero assumenda, eum facere aliquid natus placeat ipsa numquam! Repellat?</p>
+      </div>
     </Layout>
   )
 }
