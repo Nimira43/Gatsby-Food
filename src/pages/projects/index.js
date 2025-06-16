941dca08daed34f16d369ae55b6478bcc27ad9f5
Nimira43 <n.rabone72@aol.com>
2025-06-16 07:59:00 +0100

    Applying projects-bg and content to projects/index.js

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -5,11 +5,12 @@ import Layout from '../../components/Layout'
 const index = () => {
   return (
     <Layout> 
-      <h1>Projects</h1>
-      <hr />
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique sit tempore aliquam quaerat eius? Officiis laudantium eligendi fugit enim ad.</p>       
-      <Link to='/projects/project1'>Project 1</Link>
-      <Link to='/projects/project2'>Project 2</Link>
+      <div className='content projects-bg'> 
+        <h1>Projects</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique sit tempore aliquam quaerat eius? Officiis laudantium eligendi fugit enim ad.</p>       
+        <Link to='/projects/project1'>Project 1</Link>
+        <Link to='/projects/project2'>Project 2</Link>
+      </div>
     </Layout>
   )
 }
